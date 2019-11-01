c805b78100342894732c2d942d04ce3bdcd2dd59
Administrator <1294464184@qq.com>
2019-11-01 14:43:39 +0800

    首次提交

---
+++ src/main/java/com/fangzhou/asongmanager/AsongmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.fangzhou.asongmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsongmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsongmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fangzhou/asongmanager/AsongmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fangzhou.asongmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsongmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
